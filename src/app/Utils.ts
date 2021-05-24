e4af329220683fc073003776528f3f6aa32b1655
Alex <csiszaralex@gmail.com>
2021-05-24 21:40:23 +0200

    :sparkles: Adds error handler test

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,7 +1,8 @@
-import { UrlWithParsedQuery, parse } from "url";
+import { UrlWithParsedQuery, parse } from 'url';
 
 export class Utils {
-  public static parseUrl(url:string):UrlWithParsedQuery {
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    if (!url) throw new Error('Empty URL!');
     return parse(url, true);
   }
 
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,24 +1,36 @@
 import { Utils } from '../app/Utils';
 
 describe('Utils test suite', () => {
+  beforeEach(() => {});
+  beforeAll(() => {});
+
   test('first test', () => {
     const res = Utils.toUpperCase('abc');
     expect(res).toBe('ABC');
   });
 
-  test('parse url', ()=>{
+  test('parse url', () => {
     const res = Utils.parseUrl('http://loclhost:8080/login');
     expect(res.href).toBe('http://loclhost:8080/login');
     expect(res.port).toBe('8080');
     expect(res.protocol).toBe('http:');
     expect(res.query).toEqual({});
-  })
-  test('parse url with query', ()=>{
+  });
+  test('parse url with query', () => {
     const res = Utils.parseUrl('http://loclhost:8080/login?user=user&pass=pass');
     const expectedQuery = {
       user: 'user',
-      pass:'pass'
-    }
+      pass: 'pass',
+    };
     expect(res.query).toEqual(expectedQuery);
-  })
+  });
+  test('invalid URL', () => {
+    try {
+      Utils.parseUrl('');
+    }
+    catch(err){
+      expect(err).toBeInstanceOf(Error);
+      expect(err).toHaveProperty('message','Empty URL!');
+    }
+  });
 });
