b49f3fcf99e9e7bcb75d4ef3299aea9bdcf2e5fd
Alex <csiszaralex@gmail.com>
2021-05-24 21:25:04 +0200

    :wrench: Adds jest config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  verbose: true,
+};
